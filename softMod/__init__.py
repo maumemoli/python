e1efde636ce394cd786d8821385f7cac5bf0c9b9
Maurizio Memoli <mau.memoli@gmail.com>
2022-05-14 16:54:38 -0700

    init file

---
+++ softMod/__init__.py
@@ -0,0 +1,16 @@
+bl_info = {
+    "name" : "SoftMod",
+    "author" : "Maurizio Memoli",
+    "description" : "",
+    "blender" : (2, 80, 0),
+    "version" : (3, 0, 0,),
+    "location" : "",
+    "warning" : "",
+    "wiki_url": "",
+    "category" : "3D View"
+}
+
+import bpy
+from bpy.types import (PropertyGroup)
+from bpy.props import (BoolProperty, PointerProperty)
+
