98e52db5e2b57e18f7a91540460774bad56b94b2
DoubleM <mau.memoli@gmail.com>
2022-11-06 10:44:16 +0800

    version up

--- MeshDataTransfer/__init__.py
+++ MeshDataTransfer/__init__.py
@@ -10,8 +10,8 @@ bl_info = {
     "author" : "Maurizio Memoli",
     "description" : "This add on will transfer geometry data from one mesh to another based on 3 different spaces:"
                     " 'world, object, uv' also will tranfer UVs based on topology",
-    "blender" : (2, 80, 0),
-    "version" : (1, 4, 3,),
+    "blender" : (2, 93, 0),
+    "version" : (1, 4, 4,),
     "location" : "(Object Mode) Mesh > ObjectData > Mesh Data Transfer ",
     "warning" : "",
     "wiki_url": "",
